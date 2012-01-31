5a7efd0598e68051720ae7dce13557bb5992f23a
janetheffernan (Dr ) <jan@heffs.org.uk>
2012-01-31 19:40:36 +0000

    plotrl.gpd.R : made plot.rl.gpd function for rl.gpd and rl.bgpd and rl.bpotgpd objects
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/texmex@490 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/predict.gpd/R/plotrl.gpd.R
+++ branches/predict.gpd/R/plotrl.gpd.R
@@ -1,4 +1,4 @@
-plotrl.gpd <-
+plotrl.gpd <- # intended as a diagnostic for a gpd fitted with no covariates. Called by plot.gpd
 function(object, alpha = .050,
          xlab, ylab, main,
          pch= 1, ptcol =2 , cex=.75, linecol = 4 ,
@@ -29,10 +29,11 @@ function(object, alpha = .050,
     invisible(list(m=m, xm=xm))
 }
 
-plot.rl.gpd <- function(object,
+
+plot.rl.gpd <- function(object, # method for rl.(boot or b)gpd object, which may have covariates.  Plots return level for each unique row in design matrix
          xlab, ylab, main,
          pch= 1, ptcol =2 , cex=.75, linecol = 4 ,
-         cicol = 0, polycol = 15, smooth = TRUE, sameAxes=TRUE ){
+         cicol = 0, polycol = 15, smooth = TRUE, sameAxes=TRUE, type="median" ){
     if (missing(xlab) || is.null(xlab)) { xlab <- "Return period" }
     if (missing(ylab) || is.null(ylab)) { ylab <- "Return level" }
     if (missing(main) || is.null(main)) { main <- "Return Level Plot" }
@@ -40,9 +41,9 @@ plot.rl.gpd <- function(object,
     nm <- length(names(object))
     nd <- dim(object[[1]])[2]
     ncov <- length(unlist(object)) / (nm * nd)
-    if(nd == 3){
-      ValNames <- c("RL","ci.l","ci.u")
-    } else {
+    ValNames <- colnames(object[[1]])
+
+    if(length(ValNames) == 1 |  !any(ValNames == "2.5%")){
       stop("Please use ci.fit=TRUE in call to predict, to calculate confidence intervals")
     }
       
@@ -50,17 +51,29 @@ plot.rl.gpd <- function(object,
 
     m <- as.numeric(substring(dimnames(Array)[[3]],first=3))
    
-    if(sameAxes){
-       yrange <- range(Array)
-    }
     if(length(main) == 1){
       Main <- rep(main,ncov)
     } else {
       Main <- main
     }
     
+    if( class(object) == "rl.bgpd" | class(object) == "rl.bootgpd"){
+      if(casefold(type) == "median"){
+        Array <- Array[,c(2,1,3),]
+      } else if(casefold(type) == "mean") {
+        Array <- Array[,c(4,1,3),]
+      } else {
+        stop("type must be \"mean\" or \"median\" ")
+      }
+    }
+
+    if(sameAxes){
+       yrange <- range(Array)
+    }    
+  
     for(i in 1:ncov){
       xm <- t(Array[i,,])
+      
       if(!sameAxes){ 
         yrange <- range(xm)
       }
@@ -71,14 +84,21 @@ plot.rl.gpd <- function(object,
     invisible(list(m=m,xm=Array))
 }
 
-plotRLgpd <- function(m,xm,polycol,cicol,linecol,ptcol,n,xdat,pch,smooth,xlab,ylab,main,xrange,yrange){
+plot.rl.bootgpd <- plot.rl.bgpd <- plot.rl.gpd
 
-    plot(m, xm[,"RL"], log = "x", type = "n",
+plotRLgpd <- function(m,xm,polycol,cicol,linecol,ptcol,n,xdat,pch,smooth,xlab,ylab,main,xrange,yrange){ 
+# workder function - called by plotrl.gpd, plot.rl.gpd, plot.rl.bgpd
+
+    o <- order(m) # in case the return period are not in ascending order.
+    m <- m[o]
+    xm <- xm[o,]
+    
+    plot(m, xm[,1], log = "x", type = "n",
          xlim=xrange, ylim=yrange, xlab = xlab, ylab = ylab, main = main)
 
       if (smooth) {
-        splo <- spline(log(m), xm[,"ci.l"] , 200)
-        sphi <- spline(log(m), xm[,"ci.u"] , 200)
+        splo <- spline(log(m), xm[,2] , 200)
+        sphi <- spline(log(m), xm[,3] , 200)
         if ( polycol != 0 ) {
             polygon( exp(c( splo$x, rev( sphi$x ) )),
 	                       c( splo$y, rev( sphi$y ) ),
@@ -89,15 +109,15 @@ plotRLgpd <- function(m,xm,polycol,cicol,linecol,ptcol,n,xdat,pch,smooth,xlab,yl
       } else{
         if (polycol != 0){
             polygon(c( m,        rev( m)),
-                    c(xm[,"ci.l"],rev(xm[,"ci.u"])),
+                    c(xm[,2],rev(xm[,3])),
                     col=polycol, border = FALSE) # Close polygon
         } else {
-            lines(m, xm[,"ci.u"], col = cicol)
-            lines(m, xm[,"ci.l"], col = cicol)
+            lines(m, xm[,2], col = cicol)
+            lines(m, xm[,3], col = cicol)
         }
       }      
 	
-      lines(m, xm[,"RL"], col = linecol[ 1 ] )
+      lines(m, xm[,1], col = linecol[ 1 ] )
 
     # Add observed data to the plot
     if(!missing(xdat) & !missing(n)){
