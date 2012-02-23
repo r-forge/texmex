f1dfb69d04288be9d57575c5a1c1301c96c95405
janetheffernan (Dr ) <jan@heffs.org.uk>
2012-02-23 09:36:28 +0000

    plot.predict.link.gpd.R : test added; some tidying up to keep R CMD check happy.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/texmex@509 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/predict.gpd/R/plot.predict.link.gpd.R
+++ branches/predict.gpd/R/plot.predict.link.gpd.R
@@ -13,8 +13,12 @@ plot.predict.link.gpd <- function(x, main=NULL,
            cn != "phi.lo" & cn != "phi.hi" & 
            cn != "xi.lo"  & cn != "xi.hi" & 
            cn != "phi.se" & cn != "xi.se"  
-           
+          
   X <- x[,which]
+  if(is.null(dim(X))){
+     X <- matrix(X)
+     dimnames(X) <- list(dimnames(x)[[1]],dimnames(x)[[2]][which])
+  }
 
   for(i in 1:2){
     for(j in 1:dim(X)[2]){
@@ -42,14 +46,12 @@ plot.predict.link.gpd <- function(x, main=NULL,
   invisible()
 }
 
-plot.predict.link.bgpd <- function(x,type="median",...){
-
+plot.predict.link.bgpd <- function(x, type="median", ...){
 # re-format to same column structure as predict.link.gpd x
-
   if( casefold(type) == "median"){
-    x <- x[,c(2,6,1,3,5,7,9:dim(x)[2])]
+    x <- x[,c(2,6,3,4,7,8,9:dim(x)[2])]
   } else if(casefold(type) == "mean") {
-    x <- x[,c(4,8,1,3,5,7,9:dim(x)[2])]
+    x <- x[,c(1,5,3,4,7,8,9:dim(x)[2])]
   } else {
     stop("type must be \"mean\" or \"median\" ")
   }
@@ -61,3 +63,65 @@ plot.predict.link.bgpd <- function(x,type="median",...){
 
 plot.predict.link.bootgpd <- plot.predict.link.bgpd
 
+test.plot.predict.link.gpd <- function(){
+  n <- 100
+  M <- 1000
+  X <- data.frame(a = rnorm(n),b = runif(n,-0.3,0.3))
+  Y <- rgpd(n,exp(X[,1]),X[,2])
+  X$Y <- Y
+  fit <- gpd(Y,data=X,phi=~a, xi=~b,th=0)
+  fitb <- gpd(Y,data=X,phi=~a, xi=~b,th=0,method="sim",trace=20000)
+  o <- options(warn=-1)
+  fit.boot <- bootgpd(fit,R=20,trace=30)
+  options(o)
+  
+  nx <- 3
+  M <- seq(5,1000,len=20)
+  newX <- data.frame(a=runif(nx,0,5),b=runif(nx,-0.1,0.5))
+
+  p <- predict(fit,M=M,newdata=newX,ci=TRUE)
+  pb <- predict(fitb,M=M,newdata=newX,ci=TRUE)
+  pboot <- predict(fit.boot,M=M,newdata=newX,ci=TRUE)
+
+  p.lp <- predict(fit,type="lp",newdata=newX,ci=TRUE)
+  pb.lp <- predict(fitb,type="lp",newdata=newX,ci=TRUE)
+  pboot.lp <- predict(fit.boot,type="lp",newdata=newX,ci=TRUE)
+  
+  par(mfrow=c(3,3))
+  plot(p,sameAxes=FALSE,main="MLE")
+  plot(pb,sameAxes=FALSE,main="MCMC")
+  plot(pboot,sameAxes=FALSE,main="Bootstrap")
+  
+  par(mfrow=c(3,4))
+  plot(p.lp,main="MLE")
+  plot(pb.lp,main="MCMC")
+  plot(pboot.lp,main="Bootstrap")
+  
+# single covariate only:
+
+  Y <- rgpd(n,exp(X[1,1]),X[,2])
+  X$Y <- Y
+  fit <- gpd(Y,data=X,xi=~b,th=0)
+  fitb <- gpd(Y,data=X,xi=~b,th=0,method="sim",trace=20000)
+  o <- options(warn=-1)
+  fit.boot <- bootgpd(fit,R=20,trace=30) 
+  options(o)
+  
+  p <- predict(fit,M=M,newdata=newX,ci=TRUE)
+  pb <- predict(fitb,M=M,newdata=newX,ci=TRUE)
+  pboot <- predict(fit.boot,M=M,newdata=newX,ci=TRUE)
+  
+  p.lp <- predict(fit,type="lp",newdata=newX,ci=TRUE)
+  pb.lp <- predict(fitb,type="lp",newdata=newX,ci=TRUE)
+  pboot.lp <- predict(fit.boot,type="lp",newdata=newX,ci=TRUE)
+  
+  par(mfrow=c(3,3))
+  plot(p,sameAxes=FALSE,main="MLE")
+  plot(pb,sameAxes=FALSE,main="MCMC")
+  plot(pboot,sameAxes=FALSE,main="Bootstrap")
+  
+  par(mfrow=c(3,1))
+  plot(p.lp,main="MLE",polycol="cyan")
+  plot(pb.lp,main="MCMC",polycol="cyan")
+  plot(pboot.lp,main="Bootstrap",polycol="cyan")
+}
