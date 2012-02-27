4eac7c120bbca751a7e63d579e78042af657b1b3
janetheffernan (Dr ) <jan@heffs.org.uk>
2012-02-27 13:08:38 +0000

    Temporary branch created to enable merge of predict.gpd and v440 of pkg.  Merge comleted and reintegrated with thead of trunk.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/texmex@526 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/from440/R/AIC.gpd.R
+++
@@ -1,4 +0,0 @@
-AIC.gpd <- function(object, ..., k=2){
-  -2*object$loglik + k*length(coef(object))
-
-}
--- branches/from440/R/coef.bgpd.R
+++
@@ -1,8 +0,0 @@
-coef.bgpd <- function(object, ...){
-    res <- apply( object$param, 2, mean )
-    names(res) <- c(paste("phi:", colnames(object$X.phi)), 
-        paste("xi:", colnames(object$X.xi)))
-    res
-}
-coefficients.bgpd <- coef.bgpd
-
